46e34a866cb32cd337fdffcea8585388cd82dc85
l0ds4m0n3y <103957548+l0ds4m0n3y@users.noreply.github.com>
2023-10-10 15:02:10 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,19 @@
-import java.util.LinkedList;
 import java.util.ListIterator;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<String> myList = new LinkedList<>();
-        myList.add("A");        
-        myList.add("B");
-        myList.add("C");
-        myList.add("D");
+        KWLinkedList<String> myList = new KWLinkedList<>();
+        myList.add(0,"A");
+        myList.add(1,"B");
+        myList.add(2,"C");
+        myList.add(3,"D");
+        myList.add(4,"B");
+
+        ListIterator<String> iter = myList.listIterator(0);
+        System.out.println(myList.lastIndexOf("B"));
+        while(iter.hasNext()){
+            System.out.println(iter.next());
+        }
         
-        ListIterator<String> iter = myList.listIterator(myList.size()-1);
-        iter.previous();
-        iter.previous();
-        iter.next();
     }
 }
